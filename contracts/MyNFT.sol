cf00895eacf3da9afcc4f0e7bc23024cbf174592
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-08-01 19:42:58 +0530

    MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract myNFT is ERC721, Ownable {
     constructor() ERC721("myNFT", "NFT") {}
