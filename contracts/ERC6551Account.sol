db4518e018d870f1f1609fbe35ab0eb787b5fd09
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-08-01 19:45:30 +0530

    ERC6551Account.sol

--- contracts/ERC6551Account.sol
+++ contracts/ERC6551Account.sol
@@ -1,18 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/interfaces/IERC1271.sol";
-import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-
-import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import "./interfaces/IERC6551Account.sol";
-
-import "./lib/MinimalReceiver.sol";
-import "./lib/ERC6551AccountLib.sol";
+import {IERC721} from"@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+import {IERC6551Account} from "./interfaces/IERC6551Account.sol";
+import {MinimalReceiver} from"./lib/MinimalReceiver.sol";
+import {ERC6551AccountLib} from "./lib/ERC6551AccountLib.sol";
 
 contract ERC6551Account is IERC165, IERC1271, IERC6551Account {
     uint256 public nonce;
